0d9aa493f2176970a7e490a198e15592c425bb76
richard_parker <richard.huaman.27@unsch.edu.pe>
2023-02-09 14:32:31 -0500

    Dos funciones mas

--- ListasDoble.py
+++ ListasDoble.py
@@ -22,8 +22,33 @@ class Circular:
             self.ultimoDato = self.primeroLista
         else:
             datoAuxiliar= Nodo(dato)
-            datoAuxiliar.siguiente = datoAuxiliar.siguiente= Nodo(dato)
+            self.ultimoDato = datoAuxiliar.siguiente= Nodo(dato)
             self.ultimoDato.siguiente=self.primeroLista
+    def buscarNodo(self, dato):
+        datoAuxiliar = self.primeroLista
+        existe = False
+        while datoAuxiliar:
+            if datoAuxiliar.dato == dato:
+                existe = True
+                break
+            datoAuxiliar = datoAuxiliar.siguiente
+            if datoAuxiliar.siguiente == self.primeroLista:
+                break
+        return existe
+
+    def reemplazarNodo(self,datoCambiaré,nuevoDato):
+        if self.buscarNodo(datoCambiaré):
+            datoAux = self.primeroLista
+            while datoAux:
+                if datoAux.dato ==nuevoDato:
+                    datoAux.dato = nuevoDato
+                    break
+                datoAux = datoAux.siguiente
+                if datoAux.siguiente == self.primeroLista.siguiente:
+                    break
+        else:
+            raise Exception("El dato para reemplaza no existe en la lista")
+
     
 
-
+    
