69c6a12ecee1d67ad34e1d436a345879f6bb1da9
richard_parker <richard.huaman.27@unsch.edu.pe>
2023-02-09 14:54:32 -0500

    mas funciones ;-;

--- ListasDoble.py
+++ ListasDoble.py
@@ -8,6 +8,8 @@ class Circular:
         self.primeroLista = None
         self.ultimoDato = None
 
+    #agrega elemento al inicio 
+
     def agregarInicio(self,dato):
         if len(self)==0:
             self.primeroLista = self.ultimoDato = Nodo(dato)
@@ -16,6 +18,11 @@ class Circular:
             datoAuxiliar= Nodo(dato)
             datoAuxiliar.siguiente = self.primeroLista
             self.ultimoDato.siguiente = self.primeroLista
+            
+            
+    
+    # agrega elemento al final
+
     def agregarFinal(self,dato):
         if len(self) == 0:
             self.primeroLista=self.ultimoDato = Nodo(dato)
@@ -24,6 +31,9 @@ class Circular:
             datoAuxiliar= Nodo(dato)
             self.ultimoDato = datoAuxiliar.siguiente= Nodo(dato)
             self.ultimoDato.siguiente=self.primeroLista
+
+    
+    # busca el nodo por dato
     def buscarNodo(self, dato):
         datoAuxiliar = self.primeroLista
         existe = False
@@ -35,6 +45,9 @@ class Circular:
             if datoAuxiliar.siguiente == self.primeroLista:
                 break
         return existe
+
+
+    #Reemplaza el nodo datos cambiaras , dato nuevo
 
     def reemplazarNodo(self,datoCambiaré,nuevoDato):
         if self.buscarNodo(datoCambiaré):
@@ -49,6 +62,46 @@ class Circular:
         else:
             raise Exception("El dato para reemplaza no existe en la lista")
 
+    #funcion para borar nodo 
+
+    def borarNodo(self,datoBorar):
+        if self.buscarNodo(datoBorar):
+            dato1 = self.primeroLista
+            dato2 = self.primeroLista
+            while dato1:
+                if len(self)==1:
+                    self.primeroLista = None
+                    self.ultimoDato.siguiente = self.primeroLista
+                    break
+                if dato1.dato== datoBorar:
+                    if dato1 == self.primeroLista:
+                        self.primeroLista = self.primeroLista.siguiente
+                        self.ultimoDato.siguiente=self.primeroLista
+                    else:
+                        dato2.siguiente = dato1.siguiente
+                    return
+                dato2 = dato1
+                dato1 = dato1.siguiente
+            
+        else:
+            raise Exception("No se puede borar el nodo")
     
 
+    def __str__(self) -> str:
+        elementoAuxiliar = self.primeroLista
+        elementos = ""
+        while elementoAuxiliar:
+            elementos += str(elementoAuxiliar.dato) + " "
+
+            elementoAuxiliar = elementoAuxiliar.siguiente
+            if elementoAuxiliar.siguiente == self.primeroLista.siguiente:
+                break
+            return elementos
     
+    
+    
+
+
+
+
+
