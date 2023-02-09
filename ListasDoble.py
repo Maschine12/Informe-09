35f25485e414039358347389ae4b84c5bce5504a
richard_parker <richard.huaman.27@unsch.edu.pe>
2023-02-08 23:38:24 -0500

    definimos la funcion agregar al inicio

---
+++ ListasDoble.py
@@ -0,0 +1,20 @@
+class Nodo:
+    def __init__(self,dato):
+        self.dato =dato
+        self.siguiente = None
+    
+class Circular:
+    def __init__(self):
+        self.primeroLista = None
+        self.ultimoDato = None
+
+    def agregarInicio(self,dato):
+        if len(self)==0:
+            self.primeroLista = self.ultimoDato = Nodo(dato)
+            self.ultimoDato.siguiente=self.primeroLista
+        else:
+            datoAuxiliar= Nodo(dato)
+            datoAuxiliar.siguiente = self.primeroLista
+            self.ultimoDato.siguiente = self.primeroLista
+
+
